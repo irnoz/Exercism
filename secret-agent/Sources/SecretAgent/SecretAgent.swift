8ba3217bfe39671f4ce0a4cd5beff2cb2f3f582b
irakli nozadze <iraklinozadze2509@gmail.com>
2022-06-06 14:18:04 +0400

    5th problem solution

---
+++ secret-agent/Sources/SecretAgent/SecretAgent.swift
@@ -0,0 +1,18 @@
+func protectSecret(_ secret: String, withPassword password: String) -> (String) -> String {
+    func checkPassword(_ possiblePassword: String) -> String {
+        if possiblePassword == password {
+            return secret
+        } else {
+            return "Sorry. No hidden secrets here."
+        }
+    }
+    return checkPassword
+}
+
+func generateCombination(forRoom room: Int, usingFunction f: (Int) -> Int) -> (Int, Int, Int) {
+    var combination: (Int, Int, Int)
+    combination.0 = f(room)
+    combination.1 = f(combination.0)
+    combination.2 = f(combination.1)
+    return combination
+}
