cfb65b10241ceffa9479a42ab22b19b50b2bbf84
irakli nozadze <iraklinozadze2509@gmail.com>
2022-06-07 17:25:55 +0400

    lasagna recipe master

---
+++ lasagna-master/Sources/LasagnaMaster/LasagnaMaster.swift
@@ -0,0 +1,45 @@
+// TODO: define the 'remainingMinutesInOven' function
+
+import ColorSync
+
+func remainingMinutesInOven(elapsedMinutes: Int, expectedMinutesInOven: Int = 40) -> Int {
+    expectedMinutesInOven - elapsedMinutes
+}
+
+// TODO: define the 'preparationTimeInMinutes' function
+
+func preparationTimeInMinutes(layers: String...) -> Int {
+    layers.count * 2
+}
+
+// TODO: define the 'quantities' function
+
+func quantities(layers: String...) -> (noodles: Int, sauce: Double) {
+    var quantityOfNoodles: Int = 0
+    var quantityOfSouce: Double = 0
+    
+    for layer in layers {
+        if layer == "noodles" {
+           quantityOfNoodles += 3
+       } else if layer == "sauce" {
+             quantityOfSouce += 0.2
+         }
+    }
+    
+    return (quantityOfNoodles, quantityOfSouce)
+}
+
+// TODO: define the 'toOz' function
+
+func toOz(_ amount: inout(noodles: Int, sauce: Double)) {
+    amount.sauce = amount.sauce * 33.814
+}
+
+// TODO: define the 'redWine' function
+
+func redWine(layers: String...) -> Bool {
+    func countOccurence(of layer: String) -> Int {
+        layers.filter({ $0 == layer }).count
+    }
+    return countOccurence(of: "mozzarella") + countOccurence(of: "ricotta") + countOccurence(of: "béchamel") <= countOccurence(of: "sauce") + countOccurence(of: "meat")
+}
