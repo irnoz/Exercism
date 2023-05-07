f75b7d5188b26254b4003ce4a4048579751f632b
irakli nozadze <iraklinozadze2509@gmail.com>
2023-05-07 23:02:50 +0400

    improved solution

--- difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
+++ difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
@@ -1,45 +1,22 @@
 //Solution goes in Sources
 struct Squares {
     let num: Int
-    var squareOfSum: Int = 0
-    var sumOfSquares: Int = 0
-    var differenceOfSquares: Int = 0
-    
-    mutating func setSquareOfSum(upTo num: Int) {
-        for n in 1...num {
-            squareOfSum += n
-        }
-        squareOfSum = squareOfSum * squareOfSum
-    }
-    mutating func setSumOfSquares(upTo num: Int) {
-        for n in 1...num {
-            sumOfSquares += n * n
+    var squareOfSum: Int {
+        get {
+            let sum = (num * (1 + num)) / 2
+            return sum * sum
         }
     }
-    mutating func setDifferenceOfSquares(upTo num: Int) {
-        differenceOfSquares = squareOfSum - sumOfSquares
+    var sumOfSquares: Int {
+        get {
+            (num * (num + 1) * (2 * num + 1)) / 6
+        }
     }
-    
-    func mathApproach(n: Int) -> Int {
-        if n <= 1 {
-            return 0
-        }
-        var result = 0
-        
-        for i in 1...n {
-            for j in i+1..<n+1 {
-                result += i*j
-            }
-        }
-        
-        return 2*result
+    var differenceOfSquares: Int {
+        squareOfSum - sumOfSquares
     }
     
     init(_ num: Int) {
         self.num = num
-        
-        setSquareOfSum(upTo: num)
-        setSumOfSquares(upTo: num)
-        setDifferenceOfSquares(upTo :num)
     }
 }
