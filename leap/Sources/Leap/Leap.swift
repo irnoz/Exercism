27fae48f1c43ca520e6362412c739f1ae709e05a
irakli nozadze <iraklinozadze2509@gmail.com>
2023-05-09 23:25:07 +0400

    complete leap

--- leap/Sources/Leap/Leap.swift
+++ leap/Sources/Leap/Leap.swift
@@ -1,1 +1,16 @@
 //Solution goes in Sources
+//on every year that is evenly divisible by 4
+//  except every year that is evenly divisible by 100
+//    unless the year is also evenly divisible by 400
+class Year {
+    var isLeapYear: Bool = false
+    
+    init(calendarYear: Int) {
+        if calendarYear % 100 == 0 && calendarYear % 400 == 0 {
+            isLeapYear = true
+        }
+        if calendarYear % 4 == 0 && calendarYear % 100 != 0 {
+            isLeapYear = true
+        }
+    }
+}
