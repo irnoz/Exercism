c8c32b7e2e9da4a04c25554764aa5b16edd27214
irakli nozadze <iraklinozadze2509@gmail.com>
2024-04-26 16:21:24 +0400

    all-your-base better solurion

--- all-your-base/Sources/AllYourBase/AllYourBase.swift
+++ all-your-base/Sources/AllYourBase/AllYourBase.swift
@@ -15,94 +15,26 @@ struct Base {
         guard outputBase > 1 else {
             throw BaseError.invalidOutputBase
         }
-        
-        let outputDigits: [Int]
-        for (_, digit) in inputDigits.reversed().enumerated() {
-            guard digit >= 0 else {
-                throw BaseError.negativeDigit
-            }
-            guard digit < inputBase else {
-                throw BaseError.invalidPositiveDigit
-            }
+        guard inputDigits.allSatisfy({ $0 >= 0 }) else {
+            throw BaseError.negativeDigit
         }
-        let baseTenDigits: [Int]
-        if inputBase != 10 {
-            baseTenDigits = convertToBaseTen(inputBase: inputBase, inputDigits: inputDigits)
-        } else {
-            baseTenDigits = inputDigits
-        }
-        if outputBase != 10 {
-            outputDigits = convertFromBaseTen(inputDigits: baseTenDigits, outputBase: outputBase)
-        } else {
-            outputDigits = baseTenDigits
+        guard inputDigits.allSatisfy({ $0 < inputBase }) else {
+            throw BaseError.invalidPositiveDigit
         }
         
-        return outputDigits.isEmpty ? [0] : outputDigits
-    }
-    
-    private static func convertToBaseTen(inputBase: Int, inputDigits: [Int]) -> [Int] {
-        var outputNumber = 0
-        for (exponent, digit) in inputDigits.reversed().enumerated() {
-            outputNumber += digit * (inputBase ** exponent)
+        let digits = inputDigits.drop { $0 == 0 }
+        
+        guard !digits.isEmpty else {
+            return [0]
         }
-        return outputNumber.toDigitsArrayWith(base: 10)
-    }
-    
-     static func convertFromBaseTen(inputDigits: [Int], outputBase: Int) -> [Int] {
+        
+        var number = digits.reduce(0) { $0 * inputBase + $1 }
         var outputDigits = [Int]()
-        var inputNumber = 0;
-        for (exponent, digit) in inputDigits.reversed().enumerated() {
-            inputNumber += digit * (10 ** exponent)
-        }
-        while (inputNumber != 0){
-            outputDigits.append(inputNumber % outputBase)
-            inputNumber /= outputBase;
-        }
         
-        return outputDigits.reversed()
-    }
-}
-
-extension Int {
-    public func toDigitsArrayWith(base: Int) -> [Int] {
-        var digits = [Int]()
-        var number = self
         while number > 0 {
-            digits.append(number % base)
-            number /= base
+            outputDigits.append(number % outputBase)
+            number /= outputBase
         }
-        return digits.reversed()
-    }
-}
-
-precedencegroup Exponentiative {
-    associativity: left
-    higherThan: MultiplicationPrecedence
-}
-
-infix operator ** : Exponentiative
-
-public func ** <N: BinaryInteger>(base: N, power: N) -> N {
-    return N.self( pow(Double(base), Double(power)) )
-}
-
-public func ** <N: BinaryFloatingPoint>(base: N, power: N) -> N {
-    return N.self ( pow(Double(base), Double(power)) )
-}
-
-
-@main
-struct Main {
-    public static func main() {
-        do {
-            print(try Base.outputDigits(inputBase: 10, inputDigits: [1,3,9], outputBase: 11))
-        } catch {
-            print(error)
-        }
-        
-//        print(154.toDigitsArrayWith(base: 10))
-//
-//        print(Base.convertToBaseTen(inputBase: 2, inputDigits: [0,1,1,0]))
-//        print(Base.convertFromBaseTen(inputDigits: [138], outputBase: 6))
+        return outputDigits.reversed()
     }
 }
