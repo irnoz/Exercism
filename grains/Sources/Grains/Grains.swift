23a8e6c42120221b07be8ac7fd52a20655c7873b
irakli nozadze <iraklinozadze2509@gmail.com>
2024-04-26 17:31:04 +0400

    add better solution to grains

--- grains/Sources/Grains/Grains.swift
+++ grains/Sources/Grains/Grains.swift
@@ -1,19 +1,18 @@
-import Foundation
-
 enum GrainsError: Error {
     case inputTooLow
     case inputTooHigh
 }
 
 struct Grains {
-    static let total: UInt64 = UInt64.max
+    static var total: UInt64 {
+        get throws {
+            UInt64.max
+        }
+    }
+
     static func square(_ num: Int) throws -> UInt64 {
         guard num > 0 else { throw GrainsError.inputTooLow }
         guard num < 65 else { throw GrainsError.inputTooHigh }
-        var res: UInt64 = 1
-        for _ in 1..<num {
-            res *= 2
-        }
-        return res
+        return 1 << (num - 1)
     }
 }
