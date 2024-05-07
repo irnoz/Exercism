ad15450dc2e36bbd9356e767581b47ece255498c
irakli nozadze <iraklinozadze2509@gmail.com>
2024-05-07 17:24:46 +0400

    awesome solution for diamond

--- diamond/Sources/Diamond/Diamond.swift
+++ diamond/Sources/Diamond/Diamond.swift
@@ -1,33 +1,9 @@
-enum DiamondError: Error {
-    case invalidLetterError
-    case lowercaseLetterError
-}
-
 class Diamond {
-    private static let allLetters: [Character] = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    
     static func makeDiamond(letter: Character) -> [String] {
-        guard letter.isUppercase else {
-            print("Error: Lowercase letter not allowed") // Should throw lowercaseLetterError here
-            return []
+        let letters = String("ABCDEFGHIJKLMNOPQRSTUVWXYZ".prefix{$0 <= letter})
+        let (rows, columns) = (letters + letters.dropLast().reversed(), letters.reversed().dropLast() + letters)
+        return rows.map { (letter) in
+            columns.map { letter == $0 ? String(letter) : " "}.joined()
         }
-        guard let index = allLetters.firstIndex(of: letter) else {
-            print("Error: Invalid letter provided.") // Should throw invalidLetterError here
-            return []
-        }
-        // MARK: - Export logic to private fucntions
-        let letters = Array(allLetters[0..<index + 1])
-        let spaces = letters.count * 2 - 1
-        var letterIndex = letters.count - 1
-        var diamond = [String]()
-
-        letters.forEach { letter in
-            var letterArray = Array(repeating: " " as Character, count: spaces)
-            letterArray[letterIndex] = letter
-            letterArray[letterArray.count - letterIndex - 1] = letter
-            diamond.append(String(letterArray))
-            letterIndex -= 1
-        }
-        return diamond + diamond.reversed().dropFirst()
     }
 }
