e5aef3a0fa9a75bacb75e45aab88ab0d2e968a11
irakli nozadze <iraklinozadze2509@gmail.com>
2025-11-20 09:07:49 +0400

    add new solution for chessboard

---
+++ swift/bird-watcher/Sources/BirdWatcher/BirdWatcher.swift
@@ -0,0 +1,11 @@
+func totalBirdCount(_ birdsPerDay: [Int]) -> Int {
+    birdsPerDay.reduce(0, +)
+}
+
+func birdsInWeek(_ birdsPerDay: [Int], weekNumber: Int) -> Int {
+    birdsPerDay.dropFirst(7 * (weekNumber - 1)).prefix(7).reduce(0, +)
+}
+
+func fixBirdCountLog(_ birdsPerDay: [Int]) -> [Int] {
+    birdsPerDay.enumerated().map { $1 + 1 - $0 % 2}
+}
